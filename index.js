00169218e93f7a9bfaa4b1004a4be4a89d2a8aa7
Daniel Vadil <dvadz@yahoo.com>
2022-01-14 23:58:38 -0500

    Imported the Product model

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const path = require("path");
 const mongoose = require("mongoose");
 
+const Product = require("./models/product");
 mongoose
   .connect("mongodb://localhost:27017/farmStand")
   .then(() => {
